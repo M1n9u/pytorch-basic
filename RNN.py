996fec44d3b95818208f78c78f616ae4c4de8be1
Mingyu Jang <disrepair1825@gmail.com>
2024-07-15 00:46:57 +0900

    RNN 모델 구현
    이름 데이터를 학습하기 위한 모델을 구현하고 데이터셋에서 랜덤하게 샘플을 추출하는 함수를 구현했습니다.

--- RNN.py
+++ RNN.py
@@ -24,4 +24,48 @@ for files in filenames:
     lines = [unicodeToAscii(line) for line in lines]
     names[country_name] = lines
 
-print(country)
+import torch
+from torch import nn
+import torch.functional as F
+from torch.optim import Adam
+
+def nameToTensor(name):
+    result = torch.zeros(len(name),1,len(all_letters))
+    for idx, chr in enumerate(name):
+        result[idx][0][all_letters.find(chr)] = 1
+    return result
+
+class simpleRNN(nn.Module):
+    def __init__(self, in_channel, hidden_channel, out_channel):
+        super(simpleRNN, self).__init__()
+        self.linear = nn.Linear(in_channel, hidden_channel)
+        self.hid = nn.Linear(hidden_channel, hidden_channel)
+        self.out = nn.Linear(hidden_channel, out_channel)
+        self.hidden_channel = hidden_channel
+
+    def forward(self, x, hid):
+        hidden = F.tanh(self.linear(x)+self.hid(hid))
+        output = self.out(hidden)
+        return output
+
+    def initHidden(self):
+        return torch.zeros(1,self.hidden_channel)
+
+n_input = len(all_letters)
+n_hidden = n_input
+n_output = len(country)
+criterion = nn.CrossEntropyLoss()
+model = simpleRNN(n_input, n_hidden, n_output)
+optimizer = Adam(model.parameters(), lr=0.005, weight_decay=0.01)
+
+import random
+
+def randomSample():
+    idx = random.randint(0, len(names) - 1)
+    selected_names = list(names.values())[idx]
+    name = selected_names[random.randint(0,len(selected_names)-1)]
+    category = list(names.keys())[idx]
+    x = nameToTensor(name)
+    y = torch.tensor(country.index(category))
+    return x, y
+
